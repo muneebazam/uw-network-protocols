67ef384df386210dc66336494ca5115c9cc2ef01
Muneeb Azam <muneebazam@v1020-wn-6-76.campus-dynamic.uwaterloo.ca>
2018-09-28 15:54:14 -0400

    udp reply port

--- server.c
+++ server.c
@@ -18,7 +18,8 @@ void exception(const char *msg)
 int main(int argc, char *argv[])
 {
     // declare neccesary structs and variables
-    int sockfd;
+    int udp_sockfd;
+    int tcp_sockfd;
     int newsockfd;
     int portno = 5000;
     int req_code;
@@ -28,9 +29,13 @@ int main(int argc, char *argv[])
     int required_args = 2;
     int buffer_len = 256;
     socklen_t client_len;
+    socklen_t client_len_tcp;
     char buffer[buffer_len];
+    char *r_port_str;
     struct sockaddr_in serv_addr;
     struct sockaddr_in cli_addr;
+    struct sockaddr_in serv_addr_tcp;
+    struct sockaddr_in cli_addr_tcp;
 
     // check if correct arguments are passed and instantiate
     if (argc != required_args) {
@@ -46,8 +51,8 @@ int main(int argc, char *argv[])
 
     // continously try to open a UDP socket connection
     do {
-        sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    } while (sockfd < 0);
+        udp_sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    } while (udp_sockfd < 0);
 
     // socket configuration
     bzero((char *) &serv_addr, sizeof(serv_addr));
@@ -58,30 +63,60 @@ int main(int argc, char *argv[])
     do {
         portno += 1;
         serv_addr.sin_port = htons(portno);
-        bind_socket = bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+        bind_socket = bind(udp_sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
     } while (bind_socket < 0);
     printf("SERVER_PORT=%d\n", portno);
 
     // listen and accept on socket
-    listen(sockfd, 5);
+    listen(udp_sockfd, 5);
     client_len = sizeof(cli_addr);
     //try to receive some data, this is a blocking call
     do {
         bzero(buffer,256);
-        if ((recv_len = recvfrom(sockfd, buffer, buffer_len, 0, (struct sockaddr *) &cli_addr, &client_len)) < 0) {
+        if ((recv_len = recvfrom(udp_sockfd, buffer, buffer_len, 0, (struct sockaddr *) &cli_addr, &client_len)) < 0) {
            exception("recvfrom()");
         }
         //print details of the data received
         printf("Data: %s\n" , buffer);
     } while (atoi(buffer) != req_code);
     
-    //print details of the data received
-    printf("Data: %s\n" , buffer);
-         
-    //now reply the client with the same data
-    if (sendto(sockfd, buffer, recv_len, 0, (struct sockaddr*) &cli_addr, client_len) < 0) {
+    // create new TCP socket 
+    tcp_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (tcp_sockfd < 0) {
+       error("ERROR opening socket");
+    }
+    bzero((char *) &serv_addr_tcp, sizeof(serv_addr_tcp));
+    serv_addr_tcp.sin_family = AF_INET;
+    serv_addr_tcp.sin_addr.s_addr = INADDR_ANY;
+    do {
+       portno += 1;
+       serv_addr.sin_port = htons(portno);
+      bind_socket = bind(tcp_sockfd, (struct sockaddr *) &serv_addr_tcp, sizeof(serv_addr_tcp));
+    } while (bind_socket < 0);
+    sprintf(r_port_str, "%d", portno);
+
+    //now reply with the <r_port> value
+    if (sendto(udp_sockfd, r_port_str, length(r_port_str), 0, (struct sockaddr*) &cli_addr, client_len) < 0) {
         exception("sendto()");
     }
+
+
+    //  // wait off for this just deal with creation before udp reply
+    // listen(tcp_sockfd,5);
+    // client_len_tcp = sizeof(cli_addr_tcp);
+    //  newsockfd = accept(tcp_sockfd, 
+    //              (struct sockaddr *) &cli_addr_tcp, 
+    //              &client_len_tcp);
+    //  if (newsockfd < 0) 
+    //       error("ERROR on accept");
+    //  bzero(buffer,256);
+    //  success = read(newsockfd,buffer,255);
+    //  if (success < 0) error("ERROR reading from socket");
+    //  printf("Here is the message: %s\n", buffer);
+    //  success = write(newsockfd,"I got your message",18);
+    //  if (success < 0) error("ERROR writing to socket");
+    //  close(newsockfd);
+         
 
     // // read client message into buffer and print to stdout
     // bzero(buffer,256);
@@ -96,7 +131,8 @@ int main(int argc, char *argv[])
     // if (success < 0) {
     //     exception("ERROR writing to socket.\n");
     // }
-    close(newsockfd);
-    close(sockfd);
+    close(udp_sockfd);
+    close(tcp_sockfd);
+    //close(newsockfd);
     return 0; 
 }
