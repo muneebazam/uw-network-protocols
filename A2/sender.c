064be8f71f7b560f85bd6c21ec030981aef2c326
Muneeb Azam <mmazam@edu.uwaterloo.ca>
2018-10-28 19:11:40 -0400

    testing pthreads

--- A2/sender.c
+++ A2/sender.c
@@ -38,59 +38,58 @@ void *myThreadFun(void *vargp)
 // sender main
 int main(int argc, char *argv[]) {
 
-    //  UNCOMMENT THIS ENTIRE SECTION ONCE YOU FIGURE OUT PTHREADS
-    // // declare necessary structs and variables
-    // char *emulator_hostname;
-    // char *server_address;
-    // char *file_name;
-    // int emulator_port;
-    // int sender_port;
-    // int sockfd;
-    // int sockfd_tcp;
-    // int WINDOW = 10;
-    // int required_args = 5;
-    // int portno = 5000;
-    // struct hostent *server;
-    // struct sockaddr_in serv_addr;
-    // socklen_t server_len;
+    // declare necessary structs and variables
+    char *emulator_hostname;
+    char *server_address;
+    char *file_name;
+    int emulator_port;
+    int sender_port;
+    int sockfd;
+    int sockfd_tcp;
+    int WINDOW = 10;
+    int required_args = 5;
+    int portno = 5000;
+    struct hostent *server;
+    struct sockaddr_in serv_addr;
+    socklen_t server_len;
 
-    // // handle command line arguments
-    // if (argc != required_args) {
-    //     fprintf(stderr, "ERROR invalid number of arguments.\n");
-    //     fprintf(stderr, "USAGE: ./sender.sh <network emulator hostname> \
-    //                                         <Emulator UDP port to receive data at> \
-    //                                         <Sender UDP port to receive ACKs at> \
-    //                                         <name of the file to transfer>\n");
-    //     exit(1);
-    // } else {
-    //     emulator_hostname = argv[1];
-    //     emulator_port = atoi(argv[2]);
-    //     sender_port = atoi(argv[3]);
-    //     file_name = argv[4];
-    // }
-
-    // // configure hostname and UDP socket
-    // configureEmulatorHost(&server, &server_address);
-    // setupSocket(&sockfd, SOCK_DGRAM);
-    // bzero((char *) &serv_addr, sizeof(serv_addr));
-    // serv_addr.sin_family = AF_INET;
-    // bcopy((char *)server->h_addr, 
-    //      (char *)&serv_addr.sin_addr.s_addr,
-    //      server->h_length);
-    // serv_addr.sin_port = htons(portno);
-    // server_len = sizeof(serv_addr);
-
-    // // close TCP socket and exit
-    // close(sockfd_tcp);
-    // return 0;
-
-    pthread_t thread_id; 
-    printf("Before Thread\n"); 
-    pthread_create(&thread_id, NULL, myThreadFun, NULL); 
-    for (int i = 0; i < 100; i++) {
-        printf("%d\n", i);
+    // handle command line arguments
+    if (argc != required_args) {
+        fprintf(stderr, "ERROR invalid number of arguments.\n");
+        fprintf(stderr, "USAGE: ./sender.sh <network emulator hostname> \
+                                            <Emulator UDP port to receive data at> \
+                                            <Sender UDP port to receive ACKs at> \
+                                            <name of the file to transfer>\n");
+        exit(1);
+    } else {
+        emulator_hostname = argv[1];
+        emulator_port = atoi(argv[2]);
+        sender_port = atoi(argv[3]);
+        file_name = argv[4];
     }
-    pthread_join(thread_id, NULL); 
-    printf("After Thread\n"); 
+
+    // configure hostname and UDP socket
+    configureEmulatorHost(&server, &server_address);
+    setupSocket(&sockfd, SOCK_DGRAM);
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+    server_len = sizeof(serv_addr);
+
+    // close TCP socket and exit
+    close(sockfd_tcp);
     return 0;
+
+    // pthread_t thread_id; 
+    // printf("Before Thread\n"); 
+    // pthread_create(&thread_id, NULL, myThreadFun, NULL); 
+    // for (int i = 0; i < 100; i++) {
+    //     printf("%d\n", i);
+    // }
+    // pthread_join(thread_id, NULL); 
+    // printf("After Thread\n"); 
+    // return 0;
 }
