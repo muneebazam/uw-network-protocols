ea8e49aa35c7462fd63f15802da605582e3d956c
Muneeb Azam <mmazam@edu.uwaterloo.ca>
2018-10-30 21:18:25 -0400

    change to java

---
+++ A2/Receiver.java
@@ -0,0 +1,9 @@
+/* Receiver.java
+ */
+
+public class Receiver
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ A2/Sender.java
@@ -0,0 +1,9 @@
+/* Sender.java
+ */
+
+public class Sender
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
