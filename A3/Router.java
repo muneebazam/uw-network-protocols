372fd8204298decd0b848bff341f64df036429d5
Muneeb Azam <muneebazam@Muneebs-MacBook-Pro.local>
2018-11-22 18:34:58 -0500

    commit it

--- A3/Router.java
+++ A3/Router.java
@@ -2,6 +2,7 @@ import java.net.*;
 import java.io.PrintWriter;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 
 class Router {
@@ -59,7 +60,7 @@ class Router {
         outToServer.writeByte(router_id);
         outToServer.flush();
 
-        byte messageType = dIn.readByte();
+        byte messageType = inFromServer.readByte();
 
         clientSocket.close();
 
